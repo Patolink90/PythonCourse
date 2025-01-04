0546860038dd3516c7fbeffeb9b7f2afc86b53d7
Patolink90 <arreola.aburto.luis@gmail.com>
2025-01-04 02:58:08 -0600

    final version beta asistente_virtual.py

--- Dia 13/asistente_virtual.py
+++ Dia 13/asistente_virtual.py
@@ -70,6 +70,7 @@ def hablar(mensaje):
     engine.say(mensaje)
     engine.runAndWait()
 
+#informa el dia de la semana
 def pedir_dia():
 
     #Crear variable con datos de hoy
@@ -94,6 +95,98 @@ def pedir_dia():
     #decir el dia de la semana
     hablar(f'Hoy es {calendario[dia_semana]}')
 
-pedir_dia()
+
+#Informa que hora es
+def pedir_hora():
+    #crear una variable con datos de la hora
+    hora = datetime.datetime.now()
+    hora = f'En este momento son {hora.hour} horas con {hora.minute} minutos y {hora.second} segundos'
+    print(hora)
+    hablar(hora)
+
+#funcion saludo inicial
+def saludo_inicial():
+    #crear variable con datos de hora
+    hora = datetime.datetime.now()
+    if hora.hour < 6 or hora.hour > 20:
+        momento = 'Buenas noches'
+    elif 6 <= hora.hour < 13:
+        momento = 'Buenos días'
+    else:
+        momento = 'Buenas Tardes'
+    #decir el saludo
+    hablar(f"{momento}, Hola Patolin, soy Albedo, tu asistente personal, Por favor, dime en que puedo ayudarte?")
+
+def pedir_cosas():
+
+    #activar saludo inicial
+    saludo_inicial()
+
+    #variable de corte
+    comenzar = True
+
+    #loop central
+
+    while comenzar:
+        #activar el micro y guardar el pedido en un string
+
+        pedido = transformar_audio_en_texto().lower()
+
+        if 'abrir youtube' in pedido:
+            hablar('Con gusto, estoy abriendo youTube')
+            webbrowser.open("https://www.youtube.com")
+            continue
+        elif 'abrir navegador' in pedido:
+            hablar("Claro estoy en eso")
+            webbrowser.open("https://www.google.com")
+            continue
+        elif 'que día es hoy' in pedido:
+            pedir_dia()
+            continue
+        elif 'que hora es' in pedido:
+            pedir_hora()
+            continue
+        elif 'busca en wikipedia' in pedido:
+            hablar('Buscando eso en wikipedia')
+            pedido = pedido.replace('busca en wikipedia', '')
+            wikipedia.set_lang('es')
+            resultado = wikipedia.summary(pedido, sentences=1)
+            hablar('wikipedia dice lo siguiente')
+            hablar(resultado)
+            continue
+        elif 'busca en internet' in pedido:
+            hablar("Ya mismo estoy en eso")
+            pedido = pedido.replace("busca en internet", '')
+            pywhatkit.search(pedido)
+            continue
+        elif 'reproducir' in pedido:
+            hablar('Buena idea ya comienzo a reproducirlo')
+            pedido = pedido.replace('reproducir', '')
+            pywhatkit.playonyt(pedido)
+            continue
+        elif 'broma' in pedido:
+            hablar(pyjokes.get_joke('es'))
+            continue
+        elif 'precio de las acciones' in pedido:
+            accion = pedido.split('de')[-1].strip()
+            cartera = {
+                'apple': 'APPL',
+                'amazon': 'AMZN',
+                'google': 'GOOGL'
+            }
+
+            try:
+                accion_buscada = cartera[accion]
+                accion_buscada = yf.Ticker(accion_buscada)
+                precio_actual = accion_buscada.info['regularMarketPreviousClose']
+                hablar(f'La encontré, el precio de {accion} es {precio_actual}')
+                continue
+            except:
+                hablar('Perdon pero no la he encontrado')
+                continue
+        elif 'adiós' in pedido:
+            hablar("Me voy a descansar, cualquier cosa me avisas")
+            break
 
 
+pedir_cosas()
